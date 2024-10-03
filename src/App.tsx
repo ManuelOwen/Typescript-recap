1d05e236de16b0a322536bd71bda03a04f34fcbd
ManuelOwen <manuelowen5324f4e@gmail.com>
2024-10-03 23:25:54 +0300

    navbar

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar />
+      <About />
+    </>
+  )
+}
+
+export default App
